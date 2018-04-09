d0763e17410efd6415cee59d8cccf610c56cc427
Diego Lume <diego.lume.a@gmail.com>
2018-04-09 14:21:07 -0500

    tokens are added

--- lib/ludo/player.ex
+++ lib/ludo/player.ex
@@ -1,3 +1,9 @@
 defmodule Ludo.Player do
-  
+  alias Ludo.{Player, Token}
+  @enforced_keys [:tokens]
+  defstruct [:tokens]
+
+  def new() do
+    %Player{tokens: Token.new}
+  end
 end
--- lib/ludo/token.ex
+++ lib/ludo/token.ex
@@ -1,15 +1,32 @@
 defmodule Ludo.Token do
   alias __MODULE__
-  @enforced_keys [:position, :counter]
-  defstruct [:position, :counter]
+  @enforced_keys [:position, :counter, :number]
+  defstruct [:position, :counter, :number]
+  @types ~w(one two three four)
 
   @max_counter 45
 
   def new() do
-    {%Token{position: 0, counter: 0}}
+    Enum.map(@types, fn type -> %Token{position: 0, counter: 0, number: type} end)
   end
 
-  def add_counter(token = %Token{}, offset) do
+  def add_counter(tokens, number, offset) do
+    with {:ok, token = %Token{}} <- valid_token_number(tokens, number),
+      {:ok, final_token} <- valid_counter(token, offset) do
+        final_token
+    else
+      error -> error
+    end
+  end
+
+  defp valid_token_number(tokens, number) do
+    case Enum.find(tokens, fn token -> token.number == number end) do
+      token = %Token{} -> {:ok ,token}
+      nil -> {:error, :invalid_token}
+    end
+  end
+
+  defp valid_counter(token, offset) do
     if token.counter + offset <= @max_counter do
       {:ok, %Token{token | counter: token.counter + offset}}
     else
